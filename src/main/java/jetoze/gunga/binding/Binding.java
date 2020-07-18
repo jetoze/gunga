50b02589fd58f0447e1c3b4960c4c488d6d377a8
tzeth <jetoze@gmail.com>
2020-07-17 17:20:10 -0700

    Added simple Binding.oneWayBinding.

--- src/main/java/jetoze/gunga/binding/Binding.java
+++ src/main/java/jetoze/gunga/binding/Binding.java
@@ -1,7 +1,15 @@
 package jetoze.gunga.binding;
 
+import static java.util.Objects.*;
+
+import java.util.function.Consumer;
+
+import jetoze.attribut.Property;
+
 public interface Binding {
 
+    // TODO: Is there a way we can make this a class rather than an interface?
+    
     void syncUi();
     
     void syncModel();
@@ -11,4 +19,23 @@ public interface Binding {
     void setModelToUiEnabled(boolean enabled);
     
     void dispose();
+    
+    
+    public static <T> Binding oneWayBinding(Property<T> property, Consumer<? super T> ui) {
+        requireNonNull(property);
+        requireNonNull(ui);
+        return new AbstractBinding<>(property) {
+
+            @Override
+            protected void updateUi(T value) {
+                ui.accept(value);
+            }
+
+            @Override
+            protected void removeUiListener() {
+                // not applicable
+            }
+            
+        };
+    }
 }
